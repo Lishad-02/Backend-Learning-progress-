783d6b6ab5ed26c13d46c5f34d9e696229423706
Aurongojeb Lishad <162858087+Lishad-02@users.noreply.github.com>
2024-05-21 22:19:30 +0600

    Add files via upload
    PHP backend basic

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+echo "hello worrrl";
+
+?>
+<?php
+$a=3.05;
+$b=44;
+
+echo "hello worrrl <br>".$a ;
+echo "<br>".$b ;
+$c=$b-$a;
+echo "<br>".$c;
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <style>
+        .green-text {
+            color: green;
+            font-weight: bold; /* This makes the text bold */
+        }
+    </style>
+</head>
+<body>
+    <?php
+        // You can use PHP to output the text
+        echo '<p class="green-text">Your custom text goes here!</p>';
+    ?>
+</body>
+</html>
+
